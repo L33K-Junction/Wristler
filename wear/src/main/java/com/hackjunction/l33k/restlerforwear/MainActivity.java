d213a734c4747403dfff526f3d6f5441d9691363
zhiharkaVal <valeria.bazhenova@gmail.com>
2015-11-08 01:11:12 +0200

    added closing image animation

--- wear/src/main/java/com/hackjunction/l33k/restlerforwear/MainActivity.java
+++ wear/src/main/java/com/hackjunction/l33k/restlerforwear/MainActivity.java
@@ -11,19 +11,35 @@ import android.view.animation.Animation;
  */
 public class MainActivity extends Activity {
 
-    ImageView imageView;
+    ImageView imageViewStart, imageViewClosing;
 
     @Override
     protected void onCreate(Bundle instance) {
         super.onCreate(instance);
         setContentView(R.layout.welcome);
 
-        imageView = (ImageView) findViewById(R.id.imageWave);
-
+        imageViewStart = (ImageView) findViewById(R.id.imageWave);
         final Animation animRotate = AnimationUtils.loadAnimation(this, R.anim.wave);
+        imageViewStart.startAnimation(animRotate);
 
-        imageView.startAnimation(animRotate);
+
+        new Runnable() {
+            private boolean handState;
+
+            @Override
+            public void run() {
+                imageViewClosing = (ImageView)findViewById(R.id.imageClose);
+                if (!handState) {
+                    imageViewClosing.setImageResource(R.mipmap.thehand);
+                } else {
+                    imageViewClosing.setImageResource(R.mipmap.thehandbye);
+                }
+                handState = !handState;
+                imageViewClosing.postDelayed(this, 3000);
+            }
+        }.run();
     }
+
 
 
 }
